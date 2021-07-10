52d8d0500c76f2500036341b66b81c7a1738c642
gagagasp528 <gagagasp528@gmail.com>
2021-07-10 21:07:19 +0000

    Fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,40 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new
+  if  book.save
+    flash[:notice] = "Book was successfully created."
+     redirect_to book_path(book)
+  else
+    render :new
+  end
+
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(books_params)
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    blog.destroy(book_params)
+    redirect_to books_path(book)
+
+  end
+
+  private
+  def book_parms
+    params.require(:book).permit(:title, :body)
+  end
+
 end
